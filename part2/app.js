dd358067e372707bb7b8234262cdb05e8a7b182c
RUIHAN ZHANG <zhishengfeiji233@gmail.com>
2025-06-20 16:14:53 +0000

    [automatic commit] 2025-06-20 16:14:53 UTC

--- part2/app.js
+++ part2/app.js
@@ -20,7 +20,6 @@ const userRoutes = require('./routes/userRoutes');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
-app.use('/api/users',userRoutes)
 
 // Export the app instead of listening here
 module.exports = app;
