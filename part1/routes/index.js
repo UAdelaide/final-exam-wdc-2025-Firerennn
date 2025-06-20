a02830fa53342ed8829a29720c496de8ee56e167
RUIHAN ZHANG <zhishengfeiji233@gmail.com>
2025-06-20 09:31:48 +0000

    [automatic commit] 2025-06-20 09:31:48 UTC

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -1,15 +1,9 @@
-Firerennn:
-const express = require('express');
-const router = express.Router();
-
-Firerennn:
 const express = require('express');
 const router = express.Router();
 
 
 router.get('/walkrequests/open', async (req, res) => {
   try {
-    // 模拟数据返回，先别用 db
     const rows = [      { request_id: 1, dog_name: "Fido", requested_time: "10:00am" }
     ];
     res.json(rows);
